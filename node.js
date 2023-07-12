5f740d9114e10476340b8dff42ed383dc86e40f8
Chizo <chizoma44@gmail.com>
2023-07-12 16:30:57 +0100

    Done with the html part, and now working on the css.

---
+++ node.js
@@ -0,0 +1,33 @@
+//Calculator
+
+//Add an event listener to link all buttons with their identityies and functions.
+
+//Then perform the functions.
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "Error: cannot divide by zero";
+  } else {
+    return num1 / num2;
+  }
+}
+
+let operands = ["add", "subtract", "multiply", "divide", "error"];
+
+operands[0]();
+operands[1]();
+operands[2]();
+operands[3]();
+operands[4]();
